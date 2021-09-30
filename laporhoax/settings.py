8770b677a132e42dd152f151b13950f00bfab2ba
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2021-10-01 00:27:59 +0700

    add static

--- laporhoax/settings.py
+++ laporhoax/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,6 +141,7 @@ STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_URL = '/media/'
 
